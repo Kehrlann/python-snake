42c69c99e4d4726519fcfc067d333093204bc441
Daniel Garnier-Moiroux <git@garnier.wf>
2022-10-22 14:35:02 +0200

    add test: snake.py must not import numpy

--- test_snake.py
+++ test_snake.py
@@ -10,6 +10,14 @@ class TestSnake(unittest.TestCase):
         self.assertTrue(os.path.isfile("snake.py"),
                         "Un fichier snake.py doit exister...")
 
+    def test_does_not_have_numpy(self):
+        import_numpy = "import numpy"
+        from_numpy = "from numpy"
+        with open("snake.py", "r") as f:
+            contents = f.read()
+            self.assertTrue(
+                import_numpy not in contents and from_numpy not in contents, "Le fichier snake.py ne doit pas importer numpy")
+
     def test_has_pygame(self):
         import_pygame = "import pygame"
         from_pygame = "from pygame import"
